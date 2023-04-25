9b0201bb5de9ceca33e71895a825a178ddb183ae
yannickbrocart <mail@yannickbrocart.com>
2023-04-25 17:52:12 +0200

    Communication class

---
+++ src/java_01/testJavaCommunication.java
@@ -0,0 +1,48 @@
+package java_01;
+
+import java.util.Scanner;
+
+public class testJavaCommunication {
+
+	public static void main(String[] args) {
+
+		int a = 5, b = 25, c = 7;
+		boolean booleanLu;
+		int intLu;
+		char charLu;
+		String stringLue;
+		String ligneLue;
+
+		System.out.println("a = " + a);
+		System.out.println(b + " est la valeur de b");
+		System.out.println("c est égal à " + c);
+		System.out.println(a + " + " + b + " est égal à " + (a + b));
+
+		Scanner lectureClavier = new Scanner(System.in);
+
+		System.out.print("Entrez un boolean : ");
+		booleanLu = lectureClavier.nextBoolean();
+		System.out.println("boolean lu = " + booleanLu);
+
+		System.out.print("Entrez un integer : ");
+		intLu = lectureClavier.nextInt();
+		System.out.println("integer lu = " + intLu);
+
+		System.out.print("Entrez un caractère : ");
+		charLu = lectureClavier.next().charAt(0);
+		System.out.println("charactère lu = " + charLu);
+
+		System.out.print("Entrez une chaîne : ");
+		stringLue = lectureClavier.next();
+		System.out.println("Chaîne lue = " + stringLue);
+
+		lectureClavier.nextLine(); // Consume newline left-over
+
+		System.out.print("Entrez une ligne : ");
+		ligneLue = lectureClavier.nextLine();
+		System.out.println("Ligne lue = " + ligneLue);
+
+		lectureClavier.close();
+	}
+
+}
--- src/java_01/testJavaOperators.java
+++ src/java_01/testJavaOperators.java
@@ -4,7 +4,7 @@ public class testJavaOperators {
 
 	public static void main(String[] args) {
 
-		int a = 10, b = 6, c = 5, e = 3;
+		int a = 10, b = 055, c = 0x2d, e = 3;
 		float calc_1 = 0, calc_2 = 0, d = 2.56F;
 		a = a + b;
 		calc_1 = a * (b + c / d) % e;
@@ -15,7 +15,7 @@ public class testJavaOperators {
 		System.out.println("c = " + c);
 		System.out.println("d = " + d);
 		System.out.println("e = " + e);
-		System.out.println("calc_1 = " + (int)calc_1);
+		System.out.println("calc_1 = " + (int) calc_1);
 		System.out.println("calc_2 = " + (float) calc_2);
 	}
 
