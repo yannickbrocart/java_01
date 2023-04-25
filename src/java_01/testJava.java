eeaa20cab4dbfce4d1ae894e27e3e0c713f441d2
yannickbrocart <mail@yannickbrocart.com>
2023-04-25 15:13:54 +0200

    Variables class

--- src/java_01/testJava.java
+++
@@ -1,9 +0,0 @@
-package java_01;
-
-public class testJava {
-
-	public static void main(String[] args) {
-		System.out.println("ça marche");
-	}
-
-}
---
+++ src/java_01/testJavaVariables.java
@@ -0,0 +1,23 @@
+package java_01;
+
+public class testJavaVariables {
+
+	public static void main(String[] args) {
+
+		boolean ouiOuNon = true;
+		char alphabet_01 = 'a', alphabet_02 = 'b', alphabet_03 = 'c';
+		byte bit = 127;
+		short number = 32767;
+		int entier = 2147483647;
+		long tresLong = 2147483647;
+		float flottant = 67.3F;
+		double doubleFlottant = 67.3E4D;
+
+		System.out.println("boolean = " + ouiOuNon);
+		System.out.println("chars = " + alphabet_01 + ", " + alphabet_02 + ", " + alphabet_03);
+		System.out.println("bit / number = " + bit + " / " + number);
+		System.out.println("entier / long = " + entier + " / " + tresLong);
+		System.out.println("flottant / double = " + flottant + " / " + doubleFlottant);
+	}
+
+}
