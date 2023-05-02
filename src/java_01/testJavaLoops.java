ba561d98aa2b8c2d788246b92fa6e4fed3288332
yannickbrocart <mail@yannickbrocart.com>
2023-05-02 15:45:59 +0200

    fin loops

---
+++ src/java_01/testJavaLoops.java
@@ -0,0 +1,28 @@
+package java_01;
+
+import java.io.IOException;
+
+public class testJavaLoops {
+
+	public static void main(String[] args) throws IOException {
+
+		int compteur = 3, cible = 1;
+		do {
+			System.out.println("dans do-while Compteur est égal à " + compteur++);
+
+		} while (compteur <= cible);
+
+		compteur = 3;
+		while (compteur <= cible) {
+			System.out.println("dans while Compteur est égal à " + compteur++);
+		}
+
+		char Car = '\0';
+		String Mot = "";
+		while (Car != '\n') {
+			Car = (char) System.in.read();
+			Mot += Car;
+		}
+		System.out.print(Mot);
+	}
+}
