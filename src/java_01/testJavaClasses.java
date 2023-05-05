1049f4391fd6f5bf0897d3e0bf53487870f91da7
yannickbrocart <mail@yannickbrocart.com>
2023-05-05 15:24:44 +0200

    fin Classes

--- src/java_01/testJavaClasses.java
+++ src/java_01/testJavaClasses.java
@@ -14,12 +14,16 @@ public class testJavaClasses {
 		A.setPos(10, 10);
 		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
 		System.out.println("Le rayon est de " + A.getRayon() + ".");
-		A.setRayon(20);
+		System.out.println("Quel rayon pour le cercle ?");
+		A.setRayon(lectureClavier.nextInt());
 		System.out.println("Le rayon est de " + A.getRayon() + ".");
 		A.setRayon(30);
 		A.setPos(20, 15);
 		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
 		System.out.println("Le rayon est de " + A.getRayon() + ".");
+		System.out.println("Le nombre de modification de la position du cercle est " + A.getCompteur() + ".");
+		A.setPos(40, 18);
+		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
 		System.out.println("Le nombre de modification de la position du cercle est " + A.getCompteur() + ".");
 	}
 
