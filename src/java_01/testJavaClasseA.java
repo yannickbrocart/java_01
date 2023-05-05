7534fc6b45a9cb68486087047b8b28be89711ed7
yannickbrocart <mail@yannickbrocart.com>
2023-05-05 17:16:49 +0200

    fin Objets

--- src/java_01/testJavaClasseA.java
+++ src/java_01/testJavaClasseA.java
@@ -3,8 +3,21 @@ package java_01;
 public class testJavaClasseA {
 
 	private int posX, posY;
-	private int rayon = 15;
+	public static final int RayonDeBase = 15;
+	private int rayon = RayonDeBase;
 	static int compteur = 0;
+
+	public testJavaClasseA() {
+		posX = 50;
+		posY = 50;
+		rayon = RayonDeBase;
+	}
+
+	public testJavaClasseA(int posX, int posY) {
+		this.posX = posX;
+		this.posY = posY;
+		rayon = RayonDeBase;
+	}
 
 	public void setPos(int posX, int posY) {
 		this.posX = posX;
@@ -13,7 +26,10 @@ public class testJavaClasseA {
 	}
 
 	public void setRayon(int rayon) {
-		this.rayon = rayon;
+		if (rayon > 0 && rayon < 600)
+			this.rayon = rayon;
+		else
+			this.rayon = RayonDeBase;
 	}
 
 	public int getPosX() {
@@ -30,5 +46,11 @@ public class testJavaClasseA {
 
 	public int getCompteur() {
 		return compteur;
+	}
+
+	public void inverserRayons(testJavaClasseA objet2) {
+		int tmpRayon = objet2.getRayon();
+		objet2.setRayon(this.getRayon());
+		this.setRayon(tmpRayon);
 	}
 }
--- src/java_01/testJavaObjets.java
+++ src/java_01/testJavaObjets.java
@@ -8,23 +8,36 @@ public class testJavaObjets {
 
 		Scanner lectureClavier = new Scanner(System.in);
 		testJavaClasseA A = new testJavaClasseA();
+		testJavaClasseA B = new testJavaClasseA(49, 49);
 
-		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
-		System.out.println("Le nombre de modification de la position du cercle est " + A.getCompteur() + ".");
-		A.setPos(10, 10);
-		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
-		System.out.println("Le rayon est de " + A.getRayon() + ".");
-		System.out.println("Quel rayon pour le cercle ?");
-		A.setRayon(lectureClavier.nextInt());
-		System.out.println("Le rayon est de " + A.getRayon() + ".");
-		A.setRayon(30);
-		A.setPos(20, 15);
-		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
-		System.out.println("Le rayon est de " + A.getRayon() + ".");
-		System.out.println("Le nombre de modification de la position du cercle est " + A.getCompteur() + ".");
-		A.setPos(40, 18);
-		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
-		System.out.println("Le nombre de modification de la position du cercle est " + A.getCompteur() + ".");
+		System.out.println("Le cercle A à pour coordonnées x = " + A.getPosX() + " et y =" + A.getPosY()
+				+ " et pour rayon " + A.getRayon());
+		System.out.println("Le cercle B à pour coordonnées x = " + B.getPosX() + " et y =" + B.getPosY()
+				+ " et pour rayon " + B.getRayon());
+
+		A.setPos(20, 20);
+		B.setPos(15, 15);
+		B.setRayon(120);
+		System.out.println("Le cercle A à pour coordonnées x = " + A.getPosX() + " et y =" + A.getPosY()
+				+ " et pour rayon " + A.getRayon());
+		System.out.println("Le cercle B à pour coordonnées x = " + B.getPosX() + " et y =" + B.getPosY()
+				+ " et pour rayon " + B.getRayon());
+
+		A.inverserRayons(B);
+		System.out.println("");
+		System.out.println("Inversion des rayons : ");
+		System.out.println("");
+
+		System.out.println("Le cercle A à pour coordonnées x = " + A.getPosX() + " et y =" + A.getPosY()
+				+ " et pour rayon " + A.getRayon());
+		System.out.println("Le cercle B à pour coordonnées x = " + B.getPosX() + " et y =" + B.getPosY()
+				+ " et pour rayon " + B.getRayon());
+
+		B.setRayon(1010);
+		System.out.println("Le cercle A à pour coordonnées x = " + A.getPosX() + " et y =" + A.getPosY()
+				+ " et pour rayon " + A.getRayon());
+		System.out.println("Le cercle B à pour coordonnées x = " + B.getPosX() + " et y =" + B.getPosY()
+				+ " et pour rayon " + B.getRayon());
 	}
 
 }
