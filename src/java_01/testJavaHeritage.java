eac6b1ca1b3546014af8e61d94d977aa812fc778
yannickbrocart <mail@yannickbrocart.com>
2023-05-09 14:11:49 +0200

    Heritage suite

--- src/java_01/testJavaHeritage.java
+++ src/java_01/testJavaHeritage.java
@@ -1,30 +1,19 @@
 package java_01;
 
-import java.util.Scanner;
+public class testJavaHeritage extends testJavaClasseA {
 
-public class testJavaHeritage {
+	private String col;
 
-	public static void main(String[] args) {
+	public testJavaHeritage() {
+		super;
+		col = "green";
+	}
 
-		Scanner lectureClavier = new Scanner(System.in);
-		testJavaClasseA A = new testJavaClasseA();
-
-		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
-		System.out.println("Le nombre de modification de la position du cercle est " + A.getCompteur() + ".");
-		A.setPos(10, 10);
-		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
-		System.out.println("Le rayon est de " + A.getRayon() + ".");
-		System.out.println("Quel rayon pour le cercle ?");
-		A.setRayon(lectureClavier.nextInt());
-		System.out.println("Le rayon est de " + A.getRayon() + ".");
-		A.setRayon(30);
-		A.setPos(20, 15);
-		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
-		System.out.println("Le rayon est de " + A.getRayon() + ".");
-		System.out.println("Le nombre de modification de la position du cercle est " + A.getCompteur() + ".");
-		A.setPos(40, 18);
-		System.out.println("La position du cercle est : x = " + A.getPosX() + ", y = " + A.getPosY() + ".");
-		System.out.println("Le nombre de modification de la position du cercle est " + A.getCompteur() + ".");
+	public testJavaHeritage(int posX, int posY, int rayon, String col) {
+		this.posX = posX;
+		this.posY = posY;
+		this.rayon = rayon;
+		this.col = col;
 	}
 
 }
--- src/java_01/testJavaObjets.java
+++ src/java_01/testJavaObjets.java
@@ -9,6 +9,8 @@ public class testJavaObjets {
 		Scanner lectureClavier = new Scanner(System.in);
 		testJavaClasseA A = new testJavaClasseA();
 		testJavaClasseA B = new testJavaClasseA(49, 49);
+		testJavaHeritage H1 = new testJavaHeritage();
+		testJavaHeritage H2 = new testJavaHeritage(37, 37, 12, "blue");
 
 		System.out.println("Le cercle A à pour coordonnées x = " + A.getPosX() + " et y =" + A.getPosY()
 				+ " et pour rayon " + A.getRayon());
@@ -38,6 +40,16 @@ public class testJavaObjets {
 				+ " et pour rayon " + A.getRayon());
 		System.out.println("Le cercle B à pour coordonnées x = " + B.getPosX() + " et y =" + B.getPosY()
 				+ " et pour rayon " + B.getRayon());
+
+		System.out.println("");
+		System.out.println("Héritage : ");
+		System.out.println("");
+
+		System.out.println("Le cercle A à pour coordonnées x = " + H1.getPosX() + " et y =" + H1.getPosY()
+				+ " et pour rayon " + H1.getRayon());
+		System.out.println("Le cercle B à pour coordonnées x = " + H2.getPosX() + " et y =" + H2.getPosY()
+				+ " et pour rayon " + H2.getRayon());
+
 	}
 
 }
