8158cacafe6b5884a0ce2f26ca3b6c748c08c371
yannickbrocart <mail@yannickbrocart.com>
2023-05-10 08:21:59 +0200

    tabATrier class

---
+++ src/java_01/testJavaTab.java
@@ -0,0 +1,20 @@
+package java_01;
+
+import java.util.Scanner;
+
+public class testJavaTab {
+
+	public static void main(String[] args) {
+		String[] monTableau = new String[5];
+		Scanner lectureClavier = new Scanner(System.in);
+
+		for (int i = 0; i < monTableau.length; i++) {
+			System.out.println("Entrez un nom : ");
+			monTableau[i] = lectureClavier.next();
+		}
+		for (int cell = 0; cell < monTableau.length; cell++) {
+			System.out.print(monTableau[cell] + " - ");
+		}
+
+	}
+}
---
+++ src/java_01/testJavaTabATrier.java
@@ -0,0 +1,9 @@
+package java_01;
+
+public class testJavaTabATrier {
+
+	public static void main(String[] args) {
+
+		testJavaClasseATrier etudiant = new testJavaClasseATrier();
+	}
+}
